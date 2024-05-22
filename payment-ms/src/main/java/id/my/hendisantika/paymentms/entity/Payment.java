65d3c0b9ac0b4918a472288b9c6c656fd1f92492
hendisantika <hendisantika@yahoo.co.id>
2024-05-23 05:56:46 +0700

    Add Payment🫸🌀✏️📗🐧🐳

---
+++ payment-ms/src/main/java/id/my/hendisantika/paymentms/entity/Payment.java
@@ -0,0 +1,40 @@
+package id.my.hendisantika.paymentms.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-distributed-transaction
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 5/23/24
+ * Time: 05:56
+ * To change this template use File | Settings | File Templates.
+ */
+@Entity
+@Getter
+@Setter
+public class Payment {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @Column
+    private String mode;
+
+    @Column
+    private Long orderId;
+
+    @Column
+    private double amount;
+
+    @Column
+    private String status;
+}
