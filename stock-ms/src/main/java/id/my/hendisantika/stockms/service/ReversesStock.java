ec412edebe4bc471e05b12b571f185822f67a6e6
hendisantika <hendisantika@yahoo.co.id>
2024-05-23 05:38:15 +0700

    Add reverseStock in ReversesStock🫸🌀✏️📗🐧🐳

--- stock-ms/src/main/java/id/my/hendisantika/stockms/service/ReversesStock.java
+++ stock-ms/src/main/java/id/my/hendisantika/stockms/service/ReversesStock.java
@@ -1,9 +1,13 @@
 package id.my.hendisantika.stockms.service;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import id.my.hendisantika.stockms.dto.DeliveryEvent;
 import id.my.hendisantika.stockms.dto.PaymentEvent;
+import id.my.hendisantika.stockms.entity.WareHouse;
 import id.my.hendisantika.stockms.repository.StockRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -25,4 +29,27 @@ public class ReversesStock {
     private final StockRepository stockRepository;
 
     private final KafkaTemplate<String, PaymentEvent> kafkaTemplate;
+
+    @KafkaListener(topics = "reversed-stock", groupId = "stock-group")
+    public void reverseStock(String event) {
+        log.info("Inside reverse stock for order {}", event);
+
+        try {
+            DeliveryEvent deliveryEvent = new ObjectMapper().readValue(event, DeliveryEvent.class);
+
+            Iterable<WareHouse> inv = this.stockRepository.findByItem(deliveryEvent.getOrder().getItem());
+
+            inv.forEach(i -> {
+                i.setQuantity(i.getQuantity() + deliveryEvent.getOrder().getQuantity());
+                stockRepository.save(i);
+            });
+
+            PaymentEvent paymentEvent = new PaymentEvent();
+            paymentEvent.setOrder(deliveryEvent.getOrder());
+            paymentEvent.setType("PAYMENT_REVERSED");
+            kafkaTemplate.send("reversed-payments", paymentEvent);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
