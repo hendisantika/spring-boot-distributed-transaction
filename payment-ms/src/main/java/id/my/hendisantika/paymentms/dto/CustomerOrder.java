fded044b089a582a27a60cde3afef9d79c07abd1
hendisantika <hendisantika@yahoo.co.id>
2024-05-23 05:57:36 +0700

    Add CustomerOrder🫸🌀✏️📗🐧🐳

---
+++ payment-ms/src/main/java/id/my/hendisantika/paymentms/dto/CustomerOrder.java
@@ -0,0 +1,31 @@
+package id.my.hendisantika.paymentms.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-distributed-transaction
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 5/23/24
+ * Time: 05:57
+ * To change this template use File | Settings | File Templates.
+ */
+@Getter
+@Setter
+public class CustomerOrder {
+
+    private String item;
+
+    private int quantity;
+
+    private double amount;
+
+    private String paymentMode;
+
+    private Long orderId;
+
+    private String address;
+}
