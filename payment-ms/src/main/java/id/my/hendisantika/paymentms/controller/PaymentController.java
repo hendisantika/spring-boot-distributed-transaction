6f618d896df73602280344aae5014f87e2140c9a
hendisantika <hendisantika@yahoo.co.id>
2024-05-23 06:01:12 +0700

    Add processPayment in PaymentController🫸🌀✏️📗🐧🐳

--- payment-ms/src/main/java/id/my/hendisantika/paymentms/controller/PaymentController.java
+++ payment-ms/src/main/java/id/my/hendisantika/paymentms/controller/PaymentController.java
@@ -1,10 +1,16 @@
 package id.my.hendisantika.paymentms.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import id.my.hendisantika.paymentms.dto.CustomerOrder;
 import id.my.hendisantika.paymentms.dto.OrderEvent;
 import id.my.hendisantika.paymentms.dto.PaymentEvent;
+import id.my.hendisantika.paymentms.entity.Payment;
 import id.my.hendisantika.paymentms.entity.repository.PaymentRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Controller;
 
@@ -28,5 +34,36 @@ public class PaymentController {
     private final KafkaTemplate<String, PaymentEvent> kafkaTemplate;
 
     private final KafkaTemplate<String, OrderEvent> kafkaOrderTemplate;
+
+    @KafkaListener(topics = "new-orders", groupId = "orders-group")
+    public void processPayment(String event) throws JsonProcessingException {
+        log.info("Received event for payment {}", event);
+        OrderEvent orderEvent = new ObjectMapper().readValue(event, OrderEvent.class);
+
+        CustomerOrder order = orderEvent.getOrder();
+        Payment payment = new Payment();
+
+        try {
+            payment.setAmount(order.getAmount());
+            payment.setMode(order.getPaymentMode());
+            payment.setOrderId(order.getOrderId());
+            payment.setStatus("SUCCESS");
+            paymentRepository.save(payment);
+
+            PaymentEvent paymentEvent = new PaymentEvent();
+            paymentEvent.setOrder(orderEvent.getOrder());
+            paymentEvent.setType("PAYMENT_CREATED");
+            kafkaTemplate.send("new-payments", paymentEvent);
+        } catch (Exception e) {
+            payment.setOrderId(order.getOrderId());
+            payment.setStatus("FAILED");
+            paymentRepository.save(payment);
+
+            OrderEvent oe = new OrderEvent();
+            oe.setOrder(order);
+            oe.setType("ORDER_REVERSED");
+            kafkaOrderTemplate.send("reversed-orders", orderEvent);
+        }
+    }
 
 }
