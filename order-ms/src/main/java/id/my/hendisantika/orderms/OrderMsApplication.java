9e4eb6c22c7b8b669b33dd9650c1daa72b1b8946
hendisantika <hendisantika@yahoo.co.id>
2024-05-23 05:41:02 +0700

    Create order-ms🫸🌀✏️📗🐧🐳

---
+++ order-ms/src/main/java/id/my/hendisantika/orderms/OrderMsApplication.java
@@ -0,0 +1,13 @@
+package id.my.hendisantika.orderms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderMsApplication.class, args);
+	}
+
+}
