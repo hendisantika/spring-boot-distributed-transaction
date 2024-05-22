e0b29a52ec0009bdfb670e8ab17f5d051a256287
hendisantika <hendisantika@yahoo.co.id>
2024-05-23 05:42:42 +0700

    Add Order Entity🫸🌀✏️📗🐧🐳

---
+++ order-ms/src/main/java/id/my/hendisantika/orderms/entity/Order.java
@@ -0,0 +1,40 @@
+package id.my.hendisantika.orderms.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-distributed-transaction
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 5/23/24
+ * Time: 05:42
+ * To change this template use File | Settings | File Templates.
+ */
+@Getter
+@Setter
+@Entity
+public class Order {
+
+    @Id
+    @GeneratedValue
+    private long id;
+
+    @Column
+    private String item;
+
+    @Column
+    private int quantity;
+
+    @Column
+    private double amount;
+
+    @Column
+    private String status;
+}
