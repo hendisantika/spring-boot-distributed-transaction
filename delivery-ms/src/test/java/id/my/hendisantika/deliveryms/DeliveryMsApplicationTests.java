8c01655d3892229840d7c5161b0660d89edbb476
hendisantika <hendisantika@yahoo.co.id>
2024-05-23 06:40:17 +0700

    Create delivery-ms🫸🌀✏️📗🐧🐳

---
+++ delivery-ms/src/test/java/id/my/hendisantika/deliveryms/DeliveryMsApplicationTests.java
@@ -0,0 +1,13 @@
+package id.my.hendisantika.deliveryms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeliveryMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
