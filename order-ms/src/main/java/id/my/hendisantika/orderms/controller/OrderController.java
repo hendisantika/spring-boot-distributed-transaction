1b00ab059d8f0e3592a3c8eeb624cbd2cb907871
hendisantika <hendisantika@yahoo.co.id>
2024-05-23 05:46:35 +0700

    Add createOrder in OrderController🫸🌀✏️📗🐧🐳

--- order-ms/src/main/java/id/my/hendisantika/orderms/controller/OrderController.java
+++ order-ms/src/main/java/id/my/hendisantika/orderms/controller/OrderController.java
@@ -1,9 +1,13 @@
 package id.my.hendisantika.orderms.controller;
 
+import id.my.hendisantika.orderms.dto.CustomerOrder;
 import id.my.hendisantika.orderms.dto.OrderEvent;
+import id.my.hendisantika.orderms.entity.Order;
 import id.my.hendisantika.orderms.repository.OrderRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -26,4 +30,26 @@ public class OrderController {
 
     private final KafkaTemplate<String, OrderEvent> kafkaTemplate;
 
+    @PostMapping("/orders")
+    public void createOrder(@RequestBody CustomerOrder customerOrder) {
+        Order order = new Order();
+
+        try {
+            order.setAmount(customerOrder.getAmount());
+            order.setItem(customerOrder.getItem());
+            order.setQuantity(customerOrder.getQuantity());
+            order.setStatus("CREATED");
+            order = orderRepository.save(order);
+
+            customerOrder.setOrderId(order.getId());
+
+            OrderEvent event = new OrderEvent();
+            event.setOrder(customerOrder);
+            event.setType("ORDER_CREATED");
+            kafkaTemplate.send("new-orders", event);
+        } catch (Exception e) {
+            order.setStatus("FAILED");
+            orderRepository.save(order);
+        }
+    }
 }
