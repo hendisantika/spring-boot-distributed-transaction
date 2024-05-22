a31f10c22e6beecdc027917077db17a2ec5a57e4
hendisantika <hendisantika@yahoo.co.id>
2024-05-23 06:43:05 +0700

    Add DeliveryRepository🫸🌀✏️📗🐧🐳

---
+++ delivery-ms/src/main/java/id/my/hendisantika/deliveryms/repository/DeliveryRepository.java
@@ -0,0 +1,17 @@
+package id.my.hendisantika.deliveryms.repository;
+
+import id.my.hendisantika.deliveryms.entity.Delivery;
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-distributed-transaction
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 5/23/24
+ * Time: 06:42
+ * To change this template use File | Settings | File Templates.
+ */
+public interface DeliveryRepository extends CrudRepository<Delivery, Long> {
+}
