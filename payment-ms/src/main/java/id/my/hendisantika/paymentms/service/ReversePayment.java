938cb77de31798de61c2770fc3f1b365c2d2d156
hendisantika <hendisantika@yahoo.co.id>
2024-05-23 05:59:51 +0700

    Add reversePayment in ReversePayment🫸🌀✏️📗🐧🐳

--- payment-ms/src/main/java/id/my/hendisantika/paymentms/service/ReversePayment.java
+++ payment-ms/src/main/java/id/my/hendisantika/paymentms/service/ReversePayment.java
@@ -1,9 +1,14 @@
 package id.my.hendisantika.paymentms.service;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import id.my.hendisantika.paymentms.dto.CustomerOrder;
 import id.my.hendisantika.paymentms.dto.OrderEvent;
+import id.my.hendisantika.paymentms.dto.PaymentEvent;
+import id.my.hendisantika.paymentms.entity.Payment;
 import id.my.hendisantika.paymentms.entity.repository.PaymentRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Component;
 
@@ -25,4 +30,29 @@ public class ReversePayment {
     private final PaymentRepository paymentRepository;
 
     private final KafkaTemplate<String, OrderEvent> kafkaTemplate;
+
+    @KafkaListener(topics = "reversed-payments", groupId = "payments-group")
+    public void reversePayment(String event) {
+        log.info("Inside reverse payment for order {}", event);
+
+        try {
+            PaymentEvent paymentEvent = new ObjectMapper().readValue(event, PaymentEvent.class);
+
+            CustomerOrder order = paymentEvent.getOrder();
+
+            Iterable<Payment> payments = this.paymentRepository.findByOrderId(order.getOrderId());
+
+            payments.forEach(p -> {
+                p.setStatus("FAILED");
+                paymentRepository.save(p);
+            });
+
+            OrderEvent orderEvent = new OrderEvent();
+            orderEvent.setOrder(paymentEvent.getOrder());
+            orderEvent.setType("ORDER_REVERSED");
+            kafkaTemplate.send("reversed-orders", orderEvent);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
