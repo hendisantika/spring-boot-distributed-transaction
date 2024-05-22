2fc6dace6c326ac0fde96b168f5d629f208e0f16
hendisantika <hendisantika@yahoo.co.id>
2024-05-23 05:55:05 +0700

    Create payment-ms🫸🌀✏️📗🐧🐳

---
+++ payment-ms/src/test/java/id/my/hendisantika/paymentms/PaymentMsApplicationTests.java
@@ -0,0 +1,13 @@
+package id.my.hendisantika.paymentms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
