7df61499cef6a852bfcbde3108383fdf902f7f33
hendisantika <hendisantika@yahoo.co.id>
2024-05-23 05:30:51 +0700

    Create stock-ms 🫸🌀✏️📗🐧🐳

---
+++ stock-ms/src/test/java/id/my/hendisantika/stockms/StockMsApplicationTests.java
@@ -0,0 +1,13 @@
+package id.my.hendisantika.stockms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
