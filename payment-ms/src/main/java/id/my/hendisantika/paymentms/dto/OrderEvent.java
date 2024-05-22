d5c50168b4bd6873bf15e16d5acf8ed4625d7580
hendisantika <hendisantika@yahoo.co.id>
2024-05-23 05:57:56 +0700

    Add OrderEvent🫸🌀✏️📗🐧🐳

---
+++ payment-ms/src/main/java/id/my/hendisantika/paymentms/dto/OrderEvent.java
@@ -0,0 +1,24 @@
+package id.my.hendisantika.paymentms.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-distributed-transaction
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 5/23/24
+ * Time: 05:57
+ * To change this template use File | Settings | File Templates.
+ */
+@Getter
+@Setter
+public class OrderEvent {
+
+    private String type;
+
+    private CustomerOrder order;
+
+}
