4394f1d8b5fd0ab05f7c3a41a2268a454e69ebc0
hendisantika <hendisantika@yahoo.co.id>
2024-05-23 05:39:41 +0700

    Add updateStock in StockController🫸🌀✏️📗🐧🐳

--- stock-ms/src/main/java/id/my/hendisantika/stockms/controller/StockController.java
+++ stock-ms/src/main/java/id/my/hendisantika/stockms/controller/StockController.java
@@ -1,10 +1,16 @@
 package id.my.hendisantika.stockms.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import id.my.hendisantika.stockms.dto.CustomerOrder;
 import id.my.hendisantika.stockms.dto.DeliveryEvent;
 import id.my.hendisantika.stockms.dto.PaymentEvent;
+import id.my.hendisantika.stockms.entity.WareHouse;
 import id.my.hendisantika.stockms.repository.StockRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -30,4 +36,39 @@ public class StockController {
     private final KafkaTemplate<String, DeliveryEvent> kafkaTemplate;
 
     private final KafkaTemplate<String, PaymentEvent> kafkaPaymentTemplate;
+
+    @KafkaListener(topics = "new-payments", groupId = "payments-group")
+    public void updateStock(String paymentEvent) throws JsonProcessingException {
+        log.info("Inside update inventory for order {}", paymentEvent);
+
+        DeliveryEvent event = new DeliveryEvent();
+
+        PaymentEvent p = new ObjectMapper().readValue(paymentEvent, PaymentEvent.class);
+        CustomerOrder order = p.getOrder();
+
+        try {
+            Iterable<WareHouse> inventories = stockRepository.findByItem(order.getItem());
+
+            boolean exists = inventories.iterator().hasNext();
+
+            if (!exists) {
+                log.info("Stock not exist so reverting the order");
+                throw new Exception("Stock not available");
+            }
+
+            inventories.forEach(i -> {
+                i.setQuantity(i.getQuantity() - order.getQuantity());
+                stockRepository.save(i);
+            });
+
+            event.setType("STOCK_UPDATED");
+            event.setOrder(p.getOrder());
+            kafkaTemplate.send("new-stock", event);
+        } catch (Exception e) {
+            PaymentEvent pe = new PaymentEvent();
+            pe.setOrder(order);
+            pe.setType("PAYMENT_REVERSED");
+            kafkaPaymentTemplate.send("reversed-payments", pe);
+        }
+    }
 }
