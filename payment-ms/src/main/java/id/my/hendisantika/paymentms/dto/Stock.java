f10e7ebc0dfabb15e9658628a3769d1e67866c93
hendisantika <hendisantika@yahoo.co.id>
2024-05-23 05:58:42 +0700

    Add Stock🫸🌀✏️📗🐧🐳

---
+++ payment-ms/src/main/java/id/my/hendisantika/paymentms/dto/Stock.java
@@ -0,0 +1,23 @@
+package id.my.hendisantika.paymentms.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-distributed-transaction
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 5/23/24
+ * Time: 05:58
+ * To change this template use File | Settings | File Templates.
+ */
+@Getter
+@Setter
+public class Stock {
+
+    private String item;
+
+    private int quantity;
+}
